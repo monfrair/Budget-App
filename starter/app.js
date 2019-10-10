ed4713f35e997faec63f73ee6c2ed0528e2c0f90
monfrair <williamsorrentino@gmail.com>
2019-10-09 21:44:33 -0400

    functions

--- starter/app.js
+++ starter/app.js
@@ -43,7 +43,7 @@ var controller = (function (budgetCtrl, UICtrl) {
 
     document.addEventListener('keypress', function (event) {
         if (event.keyCode === 13 || event.which === 13) {
-            ctrlAddItem();
+            ctrlAddItem(); //need to rewind 1 minute on video to see what/why this is****
         }
     });
 
