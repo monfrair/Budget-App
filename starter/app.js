169313384cf2e7fd87b5ff20694409e3b9e2d5c7
William Sorrentino <williamsorrentino@gmail.com>
2019-10-10 11:36:31 -0400

    building the functions

--- starter/app.js
+++ starter/app.js
@@ -14,22 +14,48 @@ var budgetController = (function () {
         this.value = value;
     };
 
-    var allExpenses = [];
-    var allIncomes = [];
-    var totalExpenses = 0;
-
     var data = {
         allItems: {
             exp: [],
             inc: []
         },
-        
+
         totals: {
             exp: 0,
-            inc: 0,
+            inc: 0
         }
-        
-    }
+
+    };
+
+    return {
+        addItem: function (type, des, val) {
+            var newItem, ID;
+
+            //ID = last ID + 1
+            // Create new ID
+            if (data.allItems[type].length > 0) {
+            ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+                
+            } else {
+                ID = 0;
+            }
+            
+            
+            //Creat new item based on inc or exp type
+            if (type === 'exp') {
+                newItem = new Expense(ID, des, val);
+            } else if (type === 'inc') {
+                newItem = new Income(ID, des, val);
+            }
+            //Push into our data structure
+            data.allItems[type].push(newItem);
+            // Return the new element
+            return newItem;
+        },
+        testing: function() {
+            console.log(data);
+        }
+    };
 
 
 })();
@@ -81,11 +107,8 @@ var controller = (function (budgetCtrl, UICtrl) {
 
     };
 
-
-
-
     var ctrlAddItem = function () {
-
+        var input, newItem;
         // to do list for this function when button is pressed.
 
         // 1. get field input data from button click
@@ -93,13 +116,13 @@ var controller = (function (budgetCtrl, UICtrl) {
         //        console.log(input);
 
         // 2. add item to budget controler
+        var newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
         // 3. add new item to user interface
 
         // 4. calculate the budget
 
         // 5. display the budget on UI
-
 
     };
 
