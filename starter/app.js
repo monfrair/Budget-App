d9497818b7f0cdecba3786c5319298854b3844c9
William Sorrentino <williamsorrentino@gmail.com>
2019-10-10 14:32:09 -0400

    updating files from office iMac

--- starter/app.js
+++ starter/app.js
@@ -78,9 +78,34 @@ var UIController = (function () {
                 type: document.querySelector(DOMstrings.inputType).value, //will be either inc or exp type from HTML
                 description: document.querySelector(DOMstrings.inputDescription).value,
 
-                value: document.querySelector(DOMstrings.inputValue).value,
+                value: document.querySelector(DOMstrings.inputValue).value
             };
         },
+        
+        addListItem: function(obj, type) {
+            var html, newHtml
+            // Create HTML string with placeholder text
+            if (type === 'inc') {
+            
+            html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+            
+            
+            } else if (type === 'exp') {
+            
+            html = '<div class="item clearfix" id="expense-%id%"><divclass="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+            }
+            // Replace the placeholder text with some actual data
+            newHtml = html.replace('%id%', obj.id);
+            newHtml = newHtml.replace('%description%', obj.description);
+            newHtml = newHtml.replace('%value%', obj.value);
+            
+            // Insert the HTML into the DOM
+            
+            
+        }
+        
+        
+        
         getDOMstrings: function () {
             return DOMstrings;
         }
@@ -88,7 +113,7 @@ var UIController = (function () {
 
 })();
 
-// Global app controler
+// Global app controler tell other modules what to do.
 var controller = (function (budgetCtrl, UICtrl) {
 
     var setupEventListeners = function () {
